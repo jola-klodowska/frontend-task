26590a06235ecb1a1230ecfc4c747a6e87897119
Jola Klodowska <jola.klodowska@wp.pl>
2022-05-22 18:48:55 +0200

    initial commit

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,14 @@
+import './Container.scss';
+import React from 'react';
+
+export interface Test {
+
+}
+
+const Container = (props:Test) => {
+    return (
+        <div></div>
+    );
+};
+
+export default Container;
